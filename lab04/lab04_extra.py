01d0ab3d53cecd0182c114c458f9c11eaf925e10
Hadon <hadonlml@gmail.com>
2021-07-23 14:34:08 +0800

    lab04

---
+++ lab04/lab04_extra.py
@@ -0,0 +1,36 @@
+""" Lab 04 Optional Questions """
+
+from lab04 import *
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    from math import sqrt
+    lst = []
+    for i in s:
+        if sqrt(i) == round(sqrt(i)):
+            lst += [round(sqrt(i))]
+    return lst
+
+
+
+
+def key_of_min_value(d):
+    """Returns the key in a dict d that corresponds to the minimum value of d.
+    >>> letters = {'a': 6, 'b': 5, 'c': 4, 'd': 5}
+    >>> min(letters)
+    'a'
+    >>> key_of_min_value(letters)
+    'c'
+    """
+    "*** YOUR CODE HERE ***"
+    return min([i for i in d], key = lambda i: d[i])
